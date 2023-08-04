ed5729db409f160c5ed142fd14890c6ef6646abe
grzesieklog <15820933+grzesieklog@users.noreply.github.com>
2023-08-04 20:13:22 +0200

    Add files via upload

--- scripts/gen-libvirt-networks.sh
+++ scripts/gen-libvirt-networks.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "net_def(swglobal)" > libvirt-netdef
-cat vlan_list | sed -E "s/(^[A-Z]{3,}[0-9]?: )//g ; s/,/\n/g ; s/([0-9]+)/net_portgroup(\1)/g" >> libvirt-netdef
-echo "net_def_end()" >> libvirt-netdef
-m4 labconf.m4 libvirt-netdef > ../hv/net/swglobal.xml
+echo "net_def(swglobal)" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
+cat vlan_list | sed -E "s/(^[A-Z]{3,}[0-9]?: )//g ; s/,/\n/g ; s/([0-9]+)/net_portgroup(\1)/g" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
+echo "net_def_end()" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
 exit
