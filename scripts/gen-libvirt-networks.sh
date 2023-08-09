c68ea7ee7e5e2e468ec2ceae1f0489daf55e3fcf
grzesieklog <15820933+grzesieklog@users.noreply.github.com>
2023-08-09 12:05:41 +0200

    Delete gen-libvirt-networks.sh

--- scripts/gen-libvirt-networks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "net_def(swglobal)" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
-cat vlan_list | sed -E "s/(^[A-Z]{3,}[0-9]?: )//g ; s/,/\n/g ; s/([0-9]+)/net_portgroup(\1)/g" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
-echo "net_def_end()" | m4 labconf.m4 - >> ../hv/net/swglobal.xml
-exit
